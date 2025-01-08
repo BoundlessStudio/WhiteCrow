e3d2d32cc6a6c94a3fdb2ab29bda46472f3dcaad
RGBKnights <master@rgbknights.com>
2025-01-08 10:48:25 -0500

    Added some throttling

--- Program.cs
+++ Program.cs
@@ -2,9 +2,33 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenAI.Chat;
+using ThrottlingTroll;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWebApplication((builderContext, workerAppBuilder) => {
+      workerAppBuilder.UseThrottlingTroll(options => {
+        options.Config = new ThrottlingTrollConfig
+        {
+          Rules =
+          [
+            new ThrottlingTrollRule
+            {
+              UriPattern = "/api/Iterator_Start",
+              LimitMethod = new FixedWindowRateLimitMethod
+              {
+                PermitLimit = 10,
+                IntervalInSeconds = 3600,
+              }
+            }
+          ]
+        };
+        options.IdentityIdExtractor = request =>
+        {
+          var httpRequest = ((IIncomingHttpRequestProxy)request).Request;
+          return httpRequest.HttpContext.Connection.RemoteIpAddress?.ToString();
+        };
+      });
+    })
     .ConfigureServices(services =>
     {
       services.AddApplicationInsightsTelemetryWorkerService();
